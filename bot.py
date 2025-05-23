3cdeb2b84cfe2a18051f76ddc3e2776758715d4d
Embarcadero13 <135007225+Embarcadero13@users.noreply.github.com>
2025-05-23 17:03:59 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 import os
 
-API_TOKEN = os.getenv("7936346773:AAE6WYPtp0POcgu_Tu8RmLhOJDlJ7-R5G2w")
+API_TOKEN = os.getenv("API_TOKEN")
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
